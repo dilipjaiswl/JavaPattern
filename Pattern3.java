20c268ba587b69845d0f192a0a940c790f756518
Dilip Kumar Jaiswal <116095783+dilipjaiswl@users.noreply.github.com>
2023-12-26 21:47:58 +0530

    Add files via upload

---
+++ Pattern3.java
@@ -0,0 +1,16 @@
+class Pattern4
+{
+  public static void main(String[] args)
+{
+for(int i= 1;i<=5;i++)
+{
+for(int j= 1;j<=5;j++)
+{
+if(j%2==0)
+System.out.print(i +" ");
+}
+
+System.out.println();
+}
+}
+}
